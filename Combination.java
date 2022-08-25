e401065653106fd04e5249b33956d5ae2ee17fda
Nik Bailey <bail0301@morris.umn.edu>
2022-08-25 13:53:17 -0500

    Moved the main method to be in the combination class
    What the title says

--- Combination.java
+++ Combination.java
@@ -5,17 +5,7 @@ import java.io.*;
 Setting up program to list all possible
  * permutations of n integers
  */
-public class main {
-    
-    public static void Permutations(int n) {
-        int[] result = new int[n];
-        for(int i = 1; i <= n; i++) {
-            result[i] = i;
-        }
-    }
-}
-
-class Combination {
+public class Combination {
     static void combinationUtil(result, int data[], int start, int end, int index, int r){
         if (index == r){
             for (int j=0; j<r; j++)
@@ -26,6 +16,16 @@ class Combination {
         for (int i=start; i<=end && end-i+1 >= r-index; i++){
             data[index] = arr[i];
             combinationUtil(arr, data, i+1, end, index+1, r);
+        }
+    }
+
+    public static void main {
+    
+        public static void Permutations(int n) {
+            int[] result = new int[n];
+            for(int i = 1; i <= n; i++) {
+                result[i] = i;
+            }
         }
     }
 }
