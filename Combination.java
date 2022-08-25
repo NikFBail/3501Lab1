78cf71656c49ab5f97f1bf0e7892bac667653b0c
Nik Bailey <bail0301@morris.umn.edu>
2022-08-25 14:22:56 -0500

    finishing lab time
    We need to be able to make it run, fix the main method, work more on editing code so it runs smoothly and efficiently

--- Combination.java
+++ Combination.java
@@ -6,47 +6,46 @@ import java.io.*;
     data[] ---> Temporary array to store current combination
     start & end ---> Starting and Ending indexes in result[]
     index  ---> Current index in data[]
-    r ---> Size of a combination to be printed */
+    result.length ---> Size of a combination to be printed */
 public class Combination {
     public static void combinationUtil(int[] result, int data[], int start, int end, int index, int r){
         
         // Current combination is ready to be printed, print it
-        if (index == r){
-            for (int j=0; j<r; j++)
+        if (index == result.length){
+            for (int j=start; j<result.length; j++)
                 System.out.print(data[j]+" ");
             System.out.println("");
             return;
         }
 
         // replace index with all possible elements. The condition
-        // "end-i+1 >= r-index" makes sure that including one element
+        // "end-i+1 >= result.length-index" makes sure that including one element
         // at index will make a combination with remaining elements
         // at remaining positions
-        for (int i=start; i<=end && end-i+1 >= r-index; i++){
+        for (int i=0; i<=end && end-i+1 >= result.length-index; i++){
             data[index] = result[i];
-            combinationUtil(result, data, i+1, end, index+1, r);
+            combinationUtil(result, data, i+1, end, index+1, result.length);
         }
     }
 
-    // The main function that prints all combinations of size r
-    // in arr[] of size n. This function mainly uses combinationUtil()
-    static void printCombination(int result[], int n, int r)
+    // The main function that prints all combinations of size result.length
+    // in result[] of size n. This function mainly uses combinationUtil()
+    static void printCombination(int result[], int n)
     {
         // A temporary array to store all combination one by one
-        int data[]=new int[r];
+        int data[]=new int[result.length];
  
         // Print all combination using temporary array 'data[]'
-        combinationUtil(result, data, 0, n-1, 0, r);
+        combinationUtil(result, data, 0, n-1, 0, result.length);
     }
  
     /*Driver function to check for above function*/
-    public static void main(int n) {
+    public static void main(String[] args) {
+            int n;
             int[] result = new int[n];
             for(int i = 1; i <= n; i++) {
                 result[i] = i;
             }
-            int r = result.length;
-            // int n = result.length;
-            printCombination(result, r, r);
+            printCombination(result, result.length);
     }
 }
