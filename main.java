7a521fd298aa59738bb87652c9ff0de4638ed4cb
Nik Bailey <bail0301@morris.umn.edu>
2022-08-25 13:25:38 -0500

    Created Array construct class
    Working on making a way to find all permutations

--- main.java
+++ main.java
@@ -1,3 +1,14 @@
+import java.util.Arrays;
+/* Lab by Ryan S and Nik B.
+Setting up program to list all possible
+ * permutations of n integers
+ */
 public class main {
-
+    
+    public static void Permutations(int n) {
+        int[] result = new int[n];
+        for(int i = 1; i <= n; i++) {
+            result[i] = i;
+        }
+    }
 }
