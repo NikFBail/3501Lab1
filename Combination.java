1e6ff0010031a03a560ded7bd77a70fc3a9d2c0b
Nik Bailey <bail0301@morris.umn.edu>
2022-08-30 10:21:10 -0500

    Changed code
    I think this code will work, but I need to put in the timer and double/triple check

--- Combination.java
+++ Combination.java
@@ -1,51 +1,56 @@
 import java.util.Arrays;
 import java.io.*;
 
-/* Lab by Ryan S and Nik B.*/
-/*  result[]  ---> Input Array
-    data[] ---> Temporary array to store current combination
-    start & end ---> Starting and Ending indexes in result[]
-    index  ---> Current index in data[]
-    result.length ---> Size of a combination to be printed */
+// Lab by Ryan S and Nik B.\
+
+// Java program to print all permutations using
+// Heap's algorithm
 public class Combination {
-    public static void combinationUtil(int[] result, int data[], int start, int end, int index){
-        
-        // Current combination is ready to be printed, print it
-        if (index == result.length){
-            for (int j=start; j<result.length; j++)
-                System.out.print(data[j]+" ");
-            System.out.println("");
-            return;
-        }
-
-        // replace index with all possible elements. The condition
-        // "end-i+1 >= result.length-index" makes sure that including one element
-        // at index will make a combination with remaining elements
-        // at remaining positions
-        for (int i=0; i<=end && end-i+1 >= result.length-index; i++){
-            data[index] = result[i];
-            combinationUtil(result, data, i+1, end, index+1);
-        }
-    }
-
-    // The main function that prints all combinations of size result.length
-    // in result[] of size n. This function mainly uses combinationUtil()
-    static void printCombination(int result[], int n)
+    // Prints the array
+    void printArr(int a[], int n)
     {
-        // A temporary array to store all combination one by one
-        int data[]=new int[result.length];
- 
-        // Print all combination using temporary array 'data[]'
-        combinationUtil(result, data, 0, n-1, 0);
+        for (int i = 0; i < n; i++)
+            System.out.print(a[i] + " ");
+        System.out.println();
     }
  
-    /*Driver function to check for above function*/
-    public static void main(String[] args) {
-            int x = 3;
-            int[] result = new int[x];
-            for(int i = 1; i <= x; i++) {
-                result[i] = i;
+    // Generating permutation using Heap Algorithm
+    void heapPermutation(int a[], int size, int n)
+    {
+        // if size becomes 1 then prints the obtained
+        // permutation
+        if (size == 1)
+            printArr(a, n);
+ 
+        for (int i = 0; i < size; i++) {
+            heapPermutation(a, size - 1, n);
+ 
+            // if size is odd, swap 0th i.e (first) and
+            // (size-1)th i.e (last) element
+            if (size % 2 == 1) {
+                int temp = a[0];
+                a[0] = a[size - 1];
+                a[size - 1] = temp;
             }
-            printCombination(result, result.length);
+ 
+            // If size is even, swap ith
+            // and (size-1)th i.e last element
+            else {
+                int temp = a[i];
+                a[i] = a[size - 1];
+                a[size - 1] = temp;
+            }
+        }
+    }
+ 
+    // Driver code
+    public static void main(String args[]) {
+        Combination obj = new Combination();
+        int x = 4;
+        int[] arr = new int[x];
+        for(int i = 0; i < x; i++) {
+            arr[i] = i + 1;
+        }
+        obj.heapPermutation(arr, arr.length, arr.length);
     }
 }
